e6999fb45bd51b3142923eed351b3fec85ba94b6
StijnDesloovere <57724700+StijnDesloovere@users.noreply.github.com>
2020-11-09 20:16:16 +0100

    Created outline for the nested loop join

--- ProjectSDS/src/main/algorithms/NestedLoopJoin.java
+++ ProjectSDS/src/main/algorithms/NestedLoopJoin.java
@@ -1,4 +1,20 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NestedLoopJoin {
+    List<List<String[]>> data;
+    List<Integer> attributes;
+
+    public NestedLoopJoin(List<List<String[]>>inputData, List<Integer> inputAttributes){
+        data = inputData;
+        attributes = inputAttributes;
+    }
+
+    public List<String[]> computeJoin(){
+        List<String[]> result = new ArrayList<>();
+
+        return result;
+    }
 }
